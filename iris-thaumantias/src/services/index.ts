1ec14f8f1ce452edc1a4bbba8b13f18619ffdcd1
Predixx <liam.patrice.berger@gmail.com>
2025-11-21 21:40:46 +0100

    feat: implement FileMonitorService for tracking file changes and updates

--- iris-thaumantias/src/services/index.ts
+++ iris-thaumantias/src/services/index.ts
@@ -1,2 +1,4 @@
 export { ArtemisWebsocketService } from './artemisWebsocketService';
 export { BuildErrorCodeLensProvider } from './buildErrorCodeLensProvider';
+export { FileMonitorService } from './fileMonitorService';
+
