337f3f6733cebcc3781d4863dfa55f5abba3c750
Predixx <liam.patrice.berger@gmail.com>
2026-01-29 01:21:26 +0100

    chore: update release scripts to use root CHANGELOG.md
    - Changed all release scripts to reference root CHANGELOG.md instead of iris-thaumantias/CHANGELOG.md
    - Affects: prepare-release.sh, create-release.sh, backfill-releases.sh, reset-releases.sh
    - Scripts now work with consolidated CHANGELOG structure

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -16,7 +16,7 @@ NC='\033[0m' # No Color
 EXTENSION_DIR="iris-thaumantias"
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 PACKAGE_JSON="${ROOT_DIR}/${EXTENSION_DIR}/package.json"
-CHANGELOG="${ROOT_DIR}/${EXTENSION_DIR}/CHANGELOG.md"
+CHANGELOG="${ROOT_DIR}/CHANGELOG.md"
 
 # Functions
 print_step() {
